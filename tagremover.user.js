97f0484f09f3ff7d6e52dc0aafb1b65844bef17a
marank <mail@m-rank.de>
2015-12-10 18:04:52 +0100

    Update to 0.4: Replace hyphens after tags as well

--- tagremover.user.js
+++ tagremover.user.js
@@ -3,7 +3,7 @@
 // @description Removes tags from post titles
 // @namespace   https://github.com/marank
 // @include     https://www.reddit.com/r/jailbreak*
-// @version     0.3
+// @version     0.4
 // @grant       none
 // ==/UserScript==
 
@@ -15,7 +15,7 @@ window.addEventListener('neverEndingLoad', function (e) {
 
 function removeTags() {
   $('a.title').not('.rtr_tagRemoved').each(function () {
-    $(this).text($(this).text().replace(/^\[[a-zA-Z]*\]\s*/, ''));
+    $(this).text($(this).text().replace(/^\[[a-zA-Z]*\][\s-]*/, ''));
     $(this).addClass('rtr_tagRemoved');
   });
 }
